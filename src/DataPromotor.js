1b6694d885877d2923654cead04ab9f026de3cdf
Helymar <helymar@outlook.com>
2022-11-17 06:35:02 -0500

    change inpunt texts

--- src/DataPromotor.js
+++ src/DataPromotor.js
@@ -4,5 +4,4 @@ import { QueryBuilder, PendingActions, Speed } from '@mui/icons-material';
 export const SidebarData = [
     { icon: Speed, route: '/' },
     { icon: PendingActions, route: '/hours' },
-    { icon: PendingActions, route: '/hourssuper' },
 ]
