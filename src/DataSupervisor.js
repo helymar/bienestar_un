0b6b62935f59eb03b3a1f3b2987db89e359ae2a1
Helymar <helymar@outlook.com>
2022-11-17 01:10:13 -0500

    Menus

---
+++ src/DataSupervisor.js
@@ -0,0 +1,7 @@
+// Sidebar
+import { QueryBuilder, Speed } from '@mui/icons-material';
+
+export const SidebarData = [
+    { icon: Speed, route: '/' },
+    { icon: QueryBuilder, route: '/supervisor' },
+]
--- src/components/FormField.jsx
+++ src/components/FormField.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 const FormField = (props) => {
     return (
-        <div className='form-inner'>
-            <label htmlFor={props.key}>{props.label}</label>
-            <input className='border-2 border-blue-800/80 rounded-md mx-2' type={props.type ?? 'input'} name={props.key} id={props.key} ref={props.ref} onChange={props.onChange}
+        <div className='flex items-start mt-2 mb-2 flex-col'>
+            <label className='text-sm mb-1 mt-1' htmlFor={props.key}>{props.label}</label>
+            <input className='h-8 w-full rounded-md border border-slate-300 text-sm pl-2 bg-transparent outline-blue-600 shadow-sm' type={props.type ?? 'input'} name={props.key} id={props.key} ref={props.ref} onChange={props.onChange}
                 value={props.value}
                 required
             />
